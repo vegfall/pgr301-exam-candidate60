58315d1bebcb8ca2a75939309ef01c0a7c8d97fa
vegfall <vefa003@student.kristiania.no>
2024-11-15 12:28:01 +0000

    Changed main.tf to include IAM role

--- infra/main.tf
+++ infra/main.tf
@@ -17,74 +17,22 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+# SQS Queue
 resource "aws_sqs_queue" "image_request_queue" {
   name = "image-request-queue-60"
 }
 
-resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda_execution_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-        Effect = "Allow"
-        Sid    = ""
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "lambda_policy"
-  description = "Policy for Lambda to access SQS, S3, and Bedrock"
-  policy      = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "sqs:ReceiveMessage",
-          "sqs:DeleteMessage",
-          "sqs:GetQueueAttributes"
-        ],
-        "Resource": "${aws_sqs_queue.image_request_queue.arn}"
-      },
-      {
-        "Effect": "Allow",
-        "Action": [
-          "s3:PutObject"
-        ],
-        "Resource": "arn:aws:s3:::pgr301-couch-explorers/60/*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": [
-          "bedrock:InvokeModel"
-        ],
-        "Resource": "*"
-      }
-    ]
-  }
-  EOF
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  policy_arn = aws_iam_policy.lambda_policy.arn
-  role       = aws_iam_role.lambda_execution_role.name
-}
-
+# Lambda Function Using Existing IAM Role
 resource "aws_lambda_function" "image_generator_lambda" {
   function_name    = "image-generator-lambda"
   filename         = "${path.module}/lambda_sqs.zip"
   handler          = "lambda_sqs.lambda_handler"
   runtime          = "python3.9"
-  role             = aws_iam_role.lambda_execution_role.arn
-  timeout          = 30
+  
+  # Use the existing IAM role ARN here
+  role = "arn:aws:iam::244530008913:role/Terraform_role_cand60"
+
+  timeout = 30
 
   environment {
     variables = {
@@ -95,6 +43,7 @@ resource "aws_lambda_function" "image_generator_lambda" {
   source_code_hash = filebase64sha256("${path.module}/lambda_sqs.zip")
 }
 
+# Event Source Mapping (Link Lambda to SQS)
 resource "aws_lambda_event_source_mapping" "sqs_to_lambda" {
   event_source_arn = aws_sqs_queue.image_request_queue.arn
   function_name    = aws_lambda_function.image_generator_lambda.arn
