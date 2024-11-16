a4cb27f9937f587f8a5a170a1a8bc31c021b23b3
vegfall <vefa003@student.kristiania.no>
2024-11-16 20:55:24 +0000

    Added dockerfile for java solution. Updated and added new github workflow

--- java_sqs_client/src/main/java/no/pgr301/_4/sqsclient/SQSClient.java
+++ java_sqs_client/src/main/java/no/pgr301/_4/sqsclient/SQSClient.java
@@ -9,8 +9,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SQSClient implements CommandLineRunner {
 
