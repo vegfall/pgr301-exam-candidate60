5e4593b5273944cb489d37e34d0df1fd675c7ce6
vegfall <vefa003@student.kristiania.no>
2024-11-17 19:21:43 +0000

    Added mock change in infra/ to trigger terraform_deploy workflow to test

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,5 @@
 variable "notification_email" {
-  description = "The email address to send alarm notifications to"
+  description = "The email address to send alarm notifications to."
   type        = string
 }
 
@@ -9,19 +9,19 @@ variable "sqs_queue_name" {
 }
 
 variable "oldest_message_threshold" {
-  description = "The threshold (in seconds) for the age of the oldest message before triggering the alarm"
+  description = "The threshold (in seconds) for the age of the oldest message before triggering the alarm."
   type        = number
   default     = 300
 }
 
 variable "alarm_period" {
-  description = "The time period (in seconds) over which the metric is evaluated"
+  description = "The time period (in seconds) over which the metric is evaluated."
   type        = number
   default     = 60
 }
 
 variable "evaluation_periods" {
-  description = "The number of periods over which the specified condition must be met"
+  description = "The number of periods over which the specified condition must be met."
   type        = number
   default     = 1
 }
